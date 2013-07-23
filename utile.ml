b970704fb337247656f959c79fc90415f68566f5
Kevin Graney <nanonet@gmail.com>
2013-07-22 22:53:13 -0400

    Add enumerate function
    Added enumerate function to Utile library.

--- utile.ml
+++ utile.ml
@@ -3,3 +3,8 @@
 let int_of_hex str =
 	int_of_string ("0x" ^ str);;
 
+let enumerate lst =
+	let rec enum count = function
+		  [] -> []
+		| h :: t -> (count, h) :: (enum (count + 1) t)
+	in enum 0 lst;;
--- utile.mli
+++ utile.mli
@@ -3,3 +3,7 @@
 
 (** [int_of_hex] converts a string of hexadecimal digits into an integer *)
 val int_of_hex: string -> int
+
+(** [enumerate] converts a list of values in a list of tuples of an integer
+	followed by that value.  Integers begin at zero and count upwards *)
+val enumerate: 'a list -> (int * 'a) list
