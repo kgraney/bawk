fdad37b643f96444bb64620717901e649dd514d1
Kevin Graney <nanonet@gmail.com>
2013-08-02 13:30:19 -0400

    Added explode and implode functions to Utile
    These are functions defined in the OCaml docs as inefficient ways
    to convert a string to a char list or a char list to a string
    respectively.

--- utile.ml
+++ utile.ml
@@ -8,3 +8,16 @@ let enumerate ?step:(step=(fun x y -> y + 1)) lst =
 		  [] -> []
 		| h :: t -> (count, h) :: (enum (step h count) t)
 	in enum 0 lst;;
+
+let explode s =
+	let rec exp i l =
+		if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+	exp (String.length s - 1) [];;
+
+let implode l =
+	let res = String.create (List.length l) in
+	let rec imp i = function
+	| [] -> res
+	| c :: l -> res.[i] <- c; imp (i + 1) l in
+	imp 0 l;;
+
--- utile.mli
+++ utile.mli
@@ -7,3 +7,9 @@ val int_of_hex: string -> int
 (** [enumerate] converts a list of values in a list of tuples of an integer
 	followed by that value.  Integers begin at zero and count upwards *)
 val enumerate: ?step:('a -> int -> int) -> 'a list -> (int * 'a) list
+
+(** [explode] converts a string into a list of characters *)
+val explode: string -> char list
+
+(** [implode] converts a list of characters into a string *)
+val implode: char list -> string
